616682f7fcae1b6462df8667ae56669a8341cd81
Wilson Santos <wkadoshi@gmail.com>
2022-07-13 17:32:38 -0300

    add project

---
+++ Models/MessageInputModel.cs
@@ -0,0 +1,10 @@
+namespace CoolMessages_RabbitMQ.Models
+{
+    public class MessageInputModel
+    {
+        public int FromId { get; set; }
+        public int ToId { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
